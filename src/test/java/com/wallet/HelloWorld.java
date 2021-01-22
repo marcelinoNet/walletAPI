8c814df9f500965756cde9dcd66113635c81c9a6
marcelinoNet <marcelinoneto34@gmail.com>
2021-01-22 11:40:58 -0300

    finalizado respositorio do usuario - testes com JUnit4

--- src/test/java/com/wallet/HelloWorld.java
+++ src/test/java/com/wallet/HelloWorld.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
